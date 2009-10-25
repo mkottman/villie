3d0c699e6d4bc3eeabaa24b28c82f92990651658
miky <miky@blackone>
2009-10-25 20:39:31 +0100

    Nicer graphics for nodes and edges

--- gui/vedge.cpp
+++ gui/vedge.cpp
@@ -1,7 +1,11 @@
 #include "vedge.h"
 
 void VEdge::render(QPainter &p) {
-    p.setBrush(Qt::yellow);
+    QRadialGradient rg(_rect.center(), _rect.height(), _rect.center());
+    rg.setColorAt(0, Qt::white);
+    rg.setColorAt(1, Qt::yellow);
+    p.setBrush(rg);
+    p.setPen(QPen(Qt::black, 2));
     p.drawRect(_rect);
-    p.drawText(_rect, name());
+    p.drawText(_rect, name(), QTextOption(Qt::AlignCenter));
 }
--- gui/vnode.cpp
+++ gui/vnode.cpp
@@ -1,8 +1,16 @@
+#include <QPainter>
+#include <QPen>
+
 #include "vnode.h"
 
+
 void VNode::render(QPainter &p) {
-    p.setBrush(Qt::white);
+    QRadialGradient rg(_rect.center(), _rect.height(), _rect.center());
+    rg.setColorAt(0, Qt::white);
+    rg.setColorAt(1, Qt::darkBlue);
+    p.setBrush(rg);
+    p.setPen(QPen(Qt::black, 2));
     p.drawEllipse(_rect);
-    p.drawText(_rect, name());
+    p.drawText(_rect, name(), QTextOption(Qt::AlignCenter));
 }
 
