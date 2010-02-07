417c52391da438b23966bab19132f12aadfe6e47
Michal Kottman <michal.kottman@gmail.com>
2010-02-07 21:49:09 +0100

    Cleanup for a fresh new start

--- gui/vector.cpp
+++
@@ -1,9 +0,0 @@
-/* 
- * File:   vector.cpp
- * Author: Michal Kottman
- * 
- * Created on October 24, 2009, 11:16 PM
- */
-
-#include "vector.h"
-
--- model/incidence.cpp
+++
@@ -1,8 +0,0 @@
-/* 
- * File:   incidence.cpp
- * Author: miky
- * 
- * Created on November 3, 2009, 2:20 PM
- */
-
-#include "incidence.h"
