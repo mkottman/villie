789950e3f6d58202d2795e6cdf67c9afcd8c9ca9
Michal Kottman <michal.kottman@gmail.com>
2010-02-16 00:11:12 +0100

    use external lqt

--- lqt/generator/dbg.cmd
+++
@@ -1,5 +0,0 @@
-setb /home/miky/projects/villie/lqt/generator/generator.lua 1201
-setb /home/miky/projects/villie/lqt/generator/generator.lua 1202
-setb /home/miky/projects/villie/lqt/generator/generator.lua 1203
-setb /home/miky/projects/villie/lqt/generator/generator.lua 1204
-run
