43d493910bb954b7079b4fee31b8f19113b5b362
Michal Kottman <michal.kottman@gmail.com>
2010-03-14 17:28:15 +0100

    simple event/hook model for following changes in graph

--- model/graph.lua
+++ model/graph.lua
@@ -1,30 +1,45 @@
 
 class.Graph()
 
+local evCreated         = event "graphCreated"
+local evLoaded          = event "graphLoaded"
+local evSaved           = event "graphSaved"
+
+local evNodeAdded       = event "nodeAdded"
+local evNodeDeleted     = event "nodeDeleted"
+local evEdgeAdded       = event "edgeAdded"
+local evEdgeDeleted     = event "edgeDeleted"
+
+local evConnected       = event "connected"
+local evDisconnected    = event "disconnected"
+
 function Graph:_init()
-    self.nodes = List()
-    self.edges = List()
+	self.nodes = List()
+	self.edges = List()
+	evCreated(self)
 end
 
 
 local next_id = 0
 local function gen_id()
-    next_id = next_id + 1
-    return next_id
+	next_id = next_id + 1
+	return next_id
 end
 
 
 function Graph:createNode(type)
-    local n = Node(gen_id(), type)
-    self.nodes:append(n)
-    return n
+	local n = Node(gen_id(), type)
+	self.nodes:append(n)
+	evNodeAdded(self, n)
+	return n
 end
 
 
 function Graph:createEdge(type)
-    local e = Edge(gen_id(), type)
-    self.edges:append(e)
-    return e
+	local e = Edge(gen_id(), type)
+	self.edges:append(e)
+	evEdgeAdded(self, e)
+	return e
 end
 
 function Graph:connect(node, edge, name, dir)
@@ -32,12 +47,16 @@ function Graph:connect(node, edge, name, dir)
 	assert(edge and edge:is_a(Edge), "edge is nil or not an Edge")
 	assert(type(name) == "string", "name is nil or not a string")
 	assert(type(dir) == "string", "dir is nil or not a string")
-    trace("[GRAPH] connecting '%s' into '%s' as '%s' (%s)", tostring(node), tostring(edge), name, dir)
-
+	
+	TODO "connect node and edge"
+	
+	evConnected(self, node, edge, name, dir)
 end
 
 function Graph:disconnect(node, edge)
-
+	TODO "disconnect node and edge"
+	
+	evDisconnected(self, node, edge)
 end
 
 --- Loads a graph from a GraphML file
@@ -54,83 +73,88 @@ function Graph:load(filename)
 		return nil, "parse error"
 	end
 
-    local edgeMap = {}
-    local nodeMap = {}
+	local edgeMap = {}
+	local nodeMap = {}
 
-    local root = doc:documentElement()
-    local graph = root:firstChild()
-    local children = graph:childNodes()
+	local root = doc:documentElement()
+	local graph = root:firstChild()
+	local children = graph:childNodes()
 
-    for i=1,children:count() do
-        local n = children:at(i-1)
-        if n:isElement() and n:nodeName():startsWith(Q"node") then
-            local e = n:toElement()
-            local name = e:attribute(Q"id")
-            if name:startsWith(Q"node") then
-                local n = self:createNode()
+	for i=1,children:count() do
+		local n = children:at(i-1)
+		if n:isElement() and n:nodeName():startsWith(Q"node") then
+			local e = n:toElement()
+			local name = e:attribute(Q"id")
+			if name:startsWith(Q"node") then
+				local n = self:createNode()
 
-                TODO "Handle attributes" --[[
-                QDomNodeList nl = e.childNodes();
-                for (int j=0; j<nl.count(); j++) {
-                    QDomNode data = nl.at(j);
-                    if (data.isElement()) {
-                        QString type = data.toElement().attribute("key");
-                        if (type == "val") {
-                            nn->setValue(Value::parse(data.firstChild().toText().data()));
-                        } else if (type == "const") {
-                            nn->setConst(data.firstChild().toText().data() == "1");
-                        }
-                    }
-                }
-                ]]
+				TODO "Handle attributes" --[[
+				QDomNodeList nl = e.childNodes();
+				for (int j=0; j<nl.count(); j++) {
+					QDomNode data = nl.at(j);
+					if (data.isElement()) {
+						QString type = data.toElement().attribute("key");
+						if (type == "val") {
+							nn->setValue(Value::parse(data.firstChild().toText().data()));
+						} else if (type == "const") {
+							nn->setConst(data.firstChild().toText().data() == "1");
+						}
+					}
+				}
+				]]
 
-                self.nodes:append(nn)
-                nodeMap[S(name)] = n
-            elseif name:startsWith(Q"edge") then
-                local e = self:createEdge()
+				self.nodes:append(nn)
+				nodeMap[S(name)] = n
+			elseif name:startsWith(Q"edge") then
+				local e = self:createEdge()
 
-                TODO "Determine edge type" --[[
-                QDomNode fc = e.firstChild();
-                QDomNode sc = fc.firstChild();
-                QString type = sc.toText().data();
-                ]]
+				TODO "Determine edge type" --[[
+				QDomNode fc = e.firstChild();
+				QDomNode sc = fc.firstChild();
+				QString type = sc.toText().data();
+				]]
 
-                self.edges:append(ee)
-                edgeMap[S(name)] = e
-            end
-        end
-    end
+				self.edges:append(ee)
+				edgeMap[S(name)] = e
+			end
+		end
+	end
 
-    for i=1,children:count() do
-        local n = children:at(i-1)
-        if n:isElement() and n:nodeName():startsWith(Q"edge") then
-            local e = n:toElement()
-            local source = e:attribute(Q"source")
-            local target = e:attribute(Q"target")
-            local name = e:firstChild():firstChild():toText():data()
+	for i=1,children:count() do
+		local n = children:at(i-1)
+		if n:isElement() and n:nodeName():startsWith(Q"edge") then
+			local e = n:toElement()
+			local source = e:attribute(Q"source")
+			local target = e:attribute(Q"target")
+			local name = e:firstChild():firstChild():toText():data()
 
-            local n, e, d
-            if source:startsWith(Q"node") then
-                n, e, d = nodeMap[S(source)], edgeMap[S(target)], 'in'
-            else
-                n, e, d = nodeMap[S(target)], edgeMap[S(source)], 'out'
-            end
+			local n, e, d
+			if source:startsWith(Q"node") then
+				n, e, d = nodeMap[S(source)], edgeMap[S(target)], 'in'
+			else
+				n, e, d = nodeMap[S(target)], edgeMap[S(source)], 'out'
+			end
 
-            self:connect(n, e, S(name), d)
-        end
-    end
+			self:connect(n, e, S(name), d)
+		end
+	end
 
-    self:dump()
-    return true
+	self:dump()
+	
+	evLoaded(self)
+	
+	return true
 end
 
 --- Saves a graph into a GraphML file
 function Graph:save(filename)
-
+	TODO "save graph"
+	
+	evSaved(self)
 end
 
 function Graph:dump()
-    trace("Graph:")
-    self.nodes:foreach(function(n) trace(" N %s", tostring(n)) end)
-    self.edges:foreach(function(e) trace(" E %s", tostring(e)) end)
+	trace("Graph:")
+	self.nodes:foreach(function(n) trace(" N %s", tostring(n)) end)
+	self.edges:foreach(function(e) trace(" E %s", tostring(e)) end)
 end
--- model/model.lua
+++ model/model.lua
@@ -1,4 +1,26 @@
 
+local hooks = {}
+
+function emit(event, ...)
+	trace(STR, 'Emitting signal', event, ...)
+	for _,h in ipairs(hooks[event]) do
+		h(...)
+	end
+end
+
+function event(name)
+	if hooks[name] then fatal('Event "%s" already defined!', name) return end
+	hooks[name] = setmetatable({}, {__call = function(t, ...)
+		emit(name, ...)
+	end})
+	return hooks[name]
+end
+
+function handle(event, handler)
+	if not hooks[event] then fatal('No event "%s" registered!', event) return end
+	table.insert(hooks[event], handler)
+end
+
 module(..., package.clean)
 
 require 'model.graph'
